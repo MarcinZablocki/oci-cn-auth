4bc38d09261bfab381a0be52dd0086ebcc6de4ba
Marcin Zablocki <32143539+MarcinZablocki@users.noreply.github.com>
2021-05-26 14:27:56 -0700

    fix retries, timeout

--- src/var/lib/oci-cn-auth/lib/metadata.py
+++ src/var/lib/oci-cn-auth/lib/metadata.py
@@ -4,34 +4,28 @@ from requests.packages.urllib3.util.retry import Retry
 
 DEFAULT_TIMEOUT = 5
 
-class TimeoutHTTPAdapter(HTTPAdapter):
-    def __init__(self, *args, **kwargs):
-        self.timeout = DEFAULT_TIMEOUT
-        if "timeout" in kwargs:
-            self.timeout = kwargs["timeout"]
-            del kwargs["timeout"]
-        super().__init__(*args, **kwargs)
-
-def get_metadata(endpoint): 
+def get_metadata(endpoint):
     """ Make a request to metadata endpoint """
 
-    retry_strategy = Retry(
-        total=3,  
-        backoff_factor=30
+    session = requests.Session()
+
+    retry = Retry(
+        total=5,
+        read=5,
+        connect=5,
+        backoff_factor=30,
         status_forcelist=[429, 500, 502, 503, 504],
-        method_whitelist=["HEAD", "GET", "OPTIONS"]
     )
 
-    adapter = HTTPAdapter(TimeoutHTTPAdapter(max_retries=retry_strategy))
-    http = requests.Session()
-    http.mount("https://", adapter)
-    http.mount("http://", adapter)
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
 
     headers = { 'Authorization' : 'Bearer Oracle' }
     metadata_url = "http://169.254.169.254/opc/"
     metadata_ver = "2"
     request_url = metadata_url + "v" + metadata_ver + "/" + endpoint
-    return http.get(request_url, headers=headers).json()
+    return session.get(request_url, headers=headers, timeout=DEFAULT_TIMEOUT).json()
 
 def get_identity():
     return get_metadata('identity')
