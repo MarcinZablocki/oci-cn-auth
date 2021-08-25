69213d98b11f149d60ba845566278ac69a12b282
Marcin Zablocki <marcin.zablocki@oracle.com>
2021-08-25 11:05:07 -0700

    Fixes from testing

--- src/var/lib/oci-cn-auth/lib/interfaces.py
+++ src/var/lib/oci-cn-auth/lib/interfaces.py
@@ -2,37 +2,6 @@ import os
 import psutil
 import socket
 from lib.config import SHAPES
-from lib.util import WpaSupplicantService
-
-
-class RdmaInterface(object): 
-    def __init__(self, interface): 
-        self.interface = interface
-        self.service = WpaSupplicantService(interface)
-
-    @property
-    def is_up(self): 
-        interfaces = psutil.net_if_stats()
-        if self.interface not in interfaces:
-            return False
-        else:
-            return interfaces[self.interface].isup
-
-    @property
-    def ips(self): 
-        interfaces = psutil.net_if_addrs()
-
-        ips = []
-
-        if self.interface not in interfaces: 
-            return ips
-
-        for link in interfaces[self.interface]:
-            if link.family == socket.AF_INET:
-                if link.address:
-                    ips.append(link.address)
-
-        return ips
 
 def get_dev_name_by_id(id): 
     """ This assumes there are no virtual interfaces """
