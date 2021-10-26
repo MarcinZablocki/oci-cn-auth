7bbb43badc634e976988990e4cf584b4aa30bfb7
GitExample <git@example.com>
2021-10-25 23:22:38 -0700

    Update timeout to 15m for network service

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -2,7 +2,7 @@
 # increase network service timeout to 15 minutes
 
 mkdir -p /etc/systemd/system/network.service.d/
-cat << EOF >> /etc/systemd/system/network.service.d/override.conf
+cat <<EOF >> /etc/systemd/system/network.service.d/override.conf
 [Service]
 Type=forking
 Restart=no
--- scripts/before-upgrade.sh
+++ scripts/before-upgrade.sh
@@ -1,3 +1,12 @@
-#systemctl=$(which systemctl)
-#${systemctl} stop oci-cn-auth.service
-#${systemctl} stop oci-cn-auth.timer
+if [ ! -f /etc/systemd/system/network.service.d/override.conf ] ; then 
+mkdir -p /etc/systemd/system/network.service.d/
+cat <<EOF >> /etc/systemd/system/network.service.d/override.conf
+[Service]
+Type=forking
+Restart=no
+TimeoutSec=15min
+EOF
+fi 
+
+systemctl=$(which systemctl)
+${systemctl} daemon-reload 
