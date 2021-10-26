d6ae6b55bed173e5b5366bc08a0ef9fc529050c9
GitExample <git@example.com>
2021-10-25 23:14:12 -0700

    Increase network service timeout to 15 minutes

--- scripts/before-remove.sh
+++ scripts/before-remove.sh
@@ -1,6 +1,10 @@
+# remove timeout override 
+rm -rf /etc/systemd/system/network.service.d/
+
 systemctl=$(which systemctl)
 ${systemctl} stop oci-cn-auth.timer
 ${systemctl} stop oci-cn-auth.service
 ${systemctl} disable oci-cn-auth.timer
 ${systemctl} disable oci-cn-auth.service
 ${systemctl} reset-failed
+
