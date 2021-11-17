5a10030af8519ff24a539694cba0cb60f6f4b5d7
Rik Kisnah <rik.kisnah@oracle.com>
2021-11-17 13:29:41 -0800

    Added version in Makefile,fixed some PEP8 issues

---
+++ scripts/before-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# increase network service timeout to 15 minutes
+
+mkdir -p /etc/systemd/system/network.service.d/
+cat <<EOF >> /etc/systemd/system/network.service.d/override.conf
+[Service]
+Type=forking
+Restart=no
+TimeoutSec=15min
+EOF
--- src/var/lib/oci-cn-auth/lib/metadata.py
+++ src/var/lib/oci-cn-auth/lib/metadata.py
@@ -1,34 +1,43 @@
+"""
+Read and return metadata
+
+"""
 import requests
 from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 
 DEFAULT_TIMEOUT = 5
 
+
 def get_metadata(endpoint):
-    """ Make a request to metadata endpoint """
+	""" Make a request to metadata endpoint """
 
-    session = requests.Session()
+	session = requests.Session()
 
-    retry = Retry(
-        total=5,
-        read=5,
-        connect=5,
-        backoff_factor=30,
-        status_forcelist=[429, 500, 502, 503, 504],
-    )
+	retry = Retry(
+		total=5,
+		read=5,
+		connect=5,
+		backoff_factor=30,
+		status_forcelist=[429, 500, 502, 503, 504],
+	)
 
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
+	adapter = HTTPAdapter(max_retries=retry)
+	session.mount('http://', adapter)
+	session.mount('https://', adapter)
 
-    headers = { 'Authorization' : 'Bearer Oracle' }
-    metadata_url = "http://169.254.169.254/opc/"
-    metadata_ver = "2"
-    request_url = metadata_url + "v" + metadata_ver + "/" + endpoint
-    return session.get(request_url, headers=headers, timeout=DEFAULT_TIMEOUT).json()
+	headers = {'Authorization': 'Bearer Oracle'}
+	metadata_url = "http://169.254.169.254/opc/"
+	metadata_ver = "2"
+	request_url = metadata_url + "v" + metadata_ver + "/" + endpoint
+	return session.get(request_url, headers=headers, timeout=DEFAULT_TIMEOUT).json()
+
 
 def get_identity():
-    return get_metadata('identity')
+	""" return identity metadata """
+	return get_metadata('identity')
+
 
 def get_instance():
-    return get_metadata('instance')
+	""" return instance metadata """
+	return get_metadata('instance')
